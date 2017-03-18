3c3b3e660e5a3e6e20a491e636b7c7ca64b5f667
chris.anatalio <anataliocs@gmail.com>
2017-03-18 16:30:33 -0700

    Initial commit

---
+++ src/main/java/com/linkedin/learning/FullStackAppAngularSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.linkedin.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackAppAngularSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackAppAngularSpringBootApplication.class, args);
+	}
+}
