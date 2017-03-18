cf33493c2f819d95d2541f83869c1ed7269954b5
chris.anatalio <anataliocs@gmail.com>
2017-03-18 16:56:42 -0700

    01_05: Configure your API using Spring Java Config

--- src/main/java/com/linkedin/learning/FullStackAppAngularSpringBootApplication.java
+++ src/main/java/com/linkedin/learning/FullStackAppAngularSpringBootApplication.java
@@ -1,9 +1,13 @@
 package com.linkedin.learning;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class FullStackAppAngularSpringBootApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/linkedin/learning/config/ApiConfig.java
@@ -0,0 +1,26 @@
+package com.linkedin.learning.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApiConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    public ObjectWriter objectWriter(ObjectMapper objectMapper) {
+        return objectMapper.writerWithDefaultPrettyPrinter();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
