c7db368145fac8eb68cddc713cb283a9544d65e1
chris.anatalio <anataliocs@gmail.com>
2017-03-21 23:35:35 -0700

    02_03: Create and style an AngularJS Component

--- src/main/webapp/angular2/src/app/app.component.spec.ts
+++ src/main/webapp/angular2/src/app/app.component.spec.ts
@@ -10,15 +10,4 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
 });
--- src/main/webapp/angular2/src/app/app.component.ts
+++ src/main/webapp/angular2/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  public submitted:boolean;
+
+  user:FormGroup;
+
+  ngOnInit() {
+    this.user = new FormGroup({
+      checkin: new FormControl(''),
+      checkout: new FormControl('')
+    });
+  }
+
+  onSubmit({value, valid}: { value:User, valid:boolean }) {
+    console.log(value);
+  }
+}
+
+export interface User {
+  checkin:string;
+  checkout:string;
 }
