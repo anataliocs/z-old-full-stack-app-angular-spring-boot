e727f0dcb1d38721bb179ca61e0d31f1607770fc
chris.anatalio <anataliocs@gmail.com>
2017-03-22 08:28:51 -0700

    02_04: Create Available Rooms Table

--- src/main/webapp/angular2/src/app/app.component.ts
+++ src/main/webapp/angular2/src/app/app.component.ts
@@ -9,21 +9,57 @@ import {FormControl, FormGroup} from "@angular/forms";
 export class AppComponent implements OnInit {
   public submitted:boolean;
 
-  user:FormGroup;
+  roomsearch:FormGroup;
+  rooms:Room[];
 
   ngOnInit() {
-    this.user = new FormGroup({
+    this.roomsearch = new FormGroup({
       checkin: new FormControl(''),
       checkout: new FormControl('')
     });
+
+    this.rooms = ROOMS;
   }
 
-  onSubmit({value, valid}: { value:User, valid:boolean }) {
+  onSubmit({value, valid}: { value:RoomSearch, valid:boolean }) {
+    console.log("Date range search:");
     console.log(value);
+  }
+
+  reserveRoom(value:string) {
+    console.log("Room id for reservation:" + value);
   }
 }
 
-export interface User {
+export interface RoomSearch {
   checkin:string;
   checkout:string;
 }
+
+export interface Room {
+  id:string,
+  roomNumber:string,
+  price:string,
+  links:string
+}
+
+var ROOMS:Room[] = [
+  {
+    "id": "37489234327",
+    "roomNumber": "406",
+    "price": "25",
+    "links": ""
+  },
+  {
+    "id": "84329874798",
+    "roomNumber": "407",
+    "price": "20",
+    "links": ""
+  },
+  {
+    "id": "17238423787",
+    "roomNumber": "408",
+    "price": "22",
+    "links": ""
+  }
+];
