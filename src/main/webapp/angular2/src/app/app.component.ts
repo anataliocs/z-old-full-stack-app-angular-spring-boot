1309c7153b256478f990d04f6fb4c1ea346cac58
chris.anatalio <anataliocs@gmail.com>
2017-03-23 09:39:29 -0700

    04_02: Implement Reserve Rooms REST API Request

--- src/main/webapp/angular2/src/app/app.component.ts
+++ src/main/webapp/angular2/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from "@angular/core";
 import {FormControl, FormGroup} from "@angular/forms";
-import {Http, Response} from "@angular/http";
+import {Http, Response, Headers, RequestOptions} from "@angular/http";
 import {Observable} from "rxjs/Rx";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/catch";
@@ -18,15 +18,28 @@ export class AppComponent implements OnInit {
   }
 
   private baseUrl:string = 'http://localhost:8080';
+  private getUrl:string = this.baseUrl + '/room/reservation/v1/';
+  private postUrl:string = this.baseUrl + '/room/reservation/v1';
   public submitted:boolean;
 
   roomsearch:FormGroup;
   rooms:Room[];
+  request:ReserveRoomRequest;
+  currentCheckInVal:string;
+  currentCheckOutVal:string;
 
   ngOnInit() {
     this.roomsearch = new FormGroup({
       checkin: new FormControl(''),
       checkout: new FormControl('')
+    });
+
+    const roomsearchValueChanges$ = this.roomsearch.valueChanges;
+
+    // subscribe to the stream
+    roomsearchValueChanges$.subscribe(x => {
+      this.currentCheckInVal = x.checkin;
+      this.currentCheckOutVal = x.checkout;
     });
   }
 
@@ -42,15 +55,27 @@ export class AppComponent implements OnInit {
   }
 
   reserveRoom(value:string) {
-    console.log("Room id for reservation:" + value);
+
+    this.request = new ReserveRoomRequest(value, this.currentCheckInVal, this.currentCheckOutVal);
+
+    this.createReservation(this.request);
   }
 
   getAll():Observable<Room[]> {
 
     //noinspection TypeScriptValidateTypes
     return this.http
-      .get(`${this.baseUrl}/room/reservation/v1/?checkin=2017-03-18&checkout=2016-03-18`)
+      .get(this.getUrl + '?checkin=' + this.currentCheckInVal + '&checkout=' + this.currentCheckOutVal)
       .map(this.mapRoom);
+  }
+
+  createReservation(body:Object) {
+    let bodyString = JSON.stringify(body); // Stringify payload
+    let headers = new Headers({'Content-Type': 'application/json'}); // ... Set content type to JSON
+    let options = new RequestOptions({headers: headers}); // Create a request option
+
+    this.http.post(this.postUrl, body, options)
+      .subscribe(res => console.log(res));
   }
 
   mapRoom(response:Response):Room[] {
@@ -62,6 +87,21 @@ export class AppComponent implements OnInit {
 export interface RoomSearch {
   checkin:string;
   checkout:string;
+}
+
+export class ReserveRoomRequest {
+  roomId:string;
+  checkin:string;
+  checkout:string;
+
+  constructor(roomId:string,
+              checkin:string,
+              checkout:string) {
+
+    this.roomId = roomId;
+    this.checkin = checkin;
+    this.checkout = checkout;
+  }
 }
 
 export interface Room {
